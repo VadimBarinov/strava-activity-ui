60afb9410cd2a2d3ae0c53a2ac4dfa7b5ad957d6
VadimBarinov <barinovvadim87@gmail.com>
2026-04-30 16:07:54 +0400

    refac render map

--- app/static/js/render_route_map.js
+++ app/static/js/render_route_map.js
@@ -1,8 +1,9 @@
 function renderRouteMap(containerId, encodedPolyline, options = {}) {
   const {
     precision = 5,
-    color = '#1a73e8',
-    weight = 5
+    color = "#FC5201",
+    weight = 5,
+    weight_border = 8
   } = options;
 
   if (!encodedPolyline) return;
@@ -10,7 +11,6 @@ function renderRouteMap(containerId, encodedPolyline, options = {}) {
   const el = document.getElementById(containerId);
   if (!el) return;
 
-  // Декодируем polyline -> [[lat,lng], ...]
   let latlngs;
   try {
     latlngs = polyline.decode(encodedPolyline, precision);
@@ -19,7 +19,6 @@ function renderRouteMap(containerId, encodedPolyline, options = {}) {
     return;
   }
 
-  // Создаём карту
   const map = L.map(containerId, {
     zoomControl: true,
     attributionControl: true
@@ -29,11 +28,38 @@ function renderRouteMap(containerId, encodedPolyline, options = {}) {
     maxZoom: 19
   }).addTo(map);
 
-  const route = L.polyline(latlngs, { color, weight }).addTo(map);
+  const routeOutline = L.polyline(latlngs, {
+    color: "#fff",
+    weight: weight_border,
+    opacity: 1
+  }).addTo(map);
+
+  const route = L.polyline(latlngs, {
+    color: color,
+    weight: weight,
+    opacity: 1
+  }).addTo(map);
+
   map.fitBounds(route.getBounds(), { padding: [20, 20] });
 
-  L.marker(latlngs[0]).addTo(map).bindPopup("Старт");
-  L.marker(latlngs[latlngs.length - 1]).addTo(map).bindPopup("Финиш");
+  L.marker(latlngs[0], { icon: dotIcon(color) }).addTo(map).bindPopup("Старт");
+  L.marker(latlngs[latlngs.length - 1], { icon: dotIcon(color) }).addTo(map).bindPopup("Финиш");
 
   return { map, route };
+}
+
+function dotIcon(color) {
+  return L.divIcon({
+    className: "",
+    iconSize: [16, 16],
+    iconAnchor: [8, 8],
+    html: `<div style="
+      width:16px;
+      height:16px;
+      border-radius:50%;
+      background:${color};
+      border:2px solid #fff;
+      box-shadow:0 1px 4px rgba(0,0,0,.45);
+    "></div>`
+  });
 }
