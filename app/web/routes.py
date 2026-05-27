f1a42ac7a723cf254afbb68b6dcb372b2f58808c
VadimBarinov <barinovvadim87@gmail.com>
2026-05-27 16:07:08 +0400

    update routes

--- app/web/routes.py
+++ app/web/routes.py
@@ -18,7 +18,7 @@ def strava_callback():
   if not code:
     return "No code returned", 400
   athlete = StravaLoginUser().login_and_get_data(code)
-  return redirect(url_for("strava_activity.athlete_activities", athlete_id=athlete.id))
+  return redirect(url_for("strava_activity.athlete_activities"))
 
 @bp.route("/logout/strava", methods=["GET"])
 def logout_strava():
@@ -28,19 +28,19 @@ def logout_strava():
 @bp.route("/", methods=["GET"])
 def index():
   if current_user.is_authenticated:
-    redirect(url_for("strava_activity.athlete_activities", athlete_id=current_user.id))
+    redirect(url_for("strava_activity.athlete_activities"))
   return render_template("index.html")
 
-@bp.route("/athlete-activities/<athlete_id>", methods=["GET"])
+@bp.route("/athlete-activities", methods=["GET"])
 # @login_required_with_token
-def athlete_activities(athlete_id: int):
+def athlete_activities():
   # activities = StravaFetchAllActivities().call(current_user.id)
   activities = StravaFetchAllActivities().call(None)
   return render_template("athlete_activities.html", activities=activities)
 
-@bp.route("/athlete-activities/<athlete_id>/activity/<activity_id>", methods=["GET"])
+@bp.route("/activity/<activity_id>", methods=["GET"])
 # @login_required_with_token
-def activity(athlete_id: int, activity_id: int):
+def activity(activity_id: int):
   # activity = StravaFetchOneActivity().call(current_user.id, activity_id)
   activity = StravaFetchOneActivity().call(None, activity_id)
   return render_template("activity.html", activity=activity)
